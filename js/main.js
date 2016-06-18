b36bb97c9345d2f4b775fa1303b1ad2a19849305
darwin <oleg.umarov@gmail.com>
2016-06-18 18:24:38 -0500

    add mongo and setup basic structure

---
+++ js/main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class Hello extends React.Component {
+  componentWillMount() {
+
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div>
+        <h3>Links</h3>
+        <ul>
+          <li>Link...</li>
+          <li>Link...</li>
+          <li>Link...</li>
+        </ul>
+      </div>
+    );
+  }
+}
